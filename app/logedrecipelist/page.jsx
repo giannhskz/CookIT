2baa069b21e9dcd147366827e9f1e9759e953389
giannhskz <giannhskz@gmail.com>
2023-01-21 18:52:55 +0200

    make loged recepies client build

--- app/logedrecipelist/page.jsx
+++ app/logedrecipelist/page.jsx
@@ -1,19 +1,55 @@
+"use client";
 import React from "react";
-export const dynamic = "force-dynamic";
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "pages/api/auth/[...nextauth]";
+// import { unstable_getServerSession } from "next-auth/next";
+// import { authOptions } from "pages/api/auth/[...nextauth]";
 import RecipeList from "./list";
-// import { useSession} from "next-auth/react";
-// import { useEffect, useState } from "react";
 
-const User = async () => {
-  const session = await unstable_getServerSession(authOptions);
-  const user = await prisma.user.findUnique({
-    where: { email: session.user.email },
-    include: { userincredients: true },
-  });
+// const User = async () => {
+//   const session = await unstable_getServerSession(authOptions);
+//   const user = await prisma.user.findUnique({
+//     where: { email: session.user.email },
+//     include: { userincredients: true },
+//   });
 
-  return <RecipeList user={user} />;
+//   return <RecipeList user={user} />;
+// };
+
+// export default User;
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+
+const User = () => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { data: session } = useSession();
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        // Make a request to the endpoint to fetch the user
+        const res = await axios.post("/api/users", {
+          email: session.user.email,
+        });
+        setUser(res.data.user);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <RecipeList user={user} />
+  );
 };
 
 export default User;
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,8 +1,8 @@
-
 import prisma from "../../../lib/prisma/prismadb";
 
 export default async (req, res) => {
   // const {id}   = req.query;
+  console.log(req.body.email);
   const user = await prisma.user.findUnique({
     where: { email: req.body.email },
     include: { userincredients: true },
